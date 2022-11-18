9c27b81a142bfb3dca797d5127d40b3171a506f0
Ana <anaisabel19886@gmail.com>
2022-11-18 09:47:13 +0000

    adds client create test

---
+++ tests/Feature/Clients/CreateTest.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Models\Client;
+use App\Models\User;
+use Illuminate\Http\UploadedFile;
+use function Pest\Laravel\actingAs;
+use \Illuminate\Support\Facades\Storage;
+
+it('client photo is stored when creating a client', function () {
+
+    Storage::fake();
+
+    $file = UploadedFile::fake()->image('client.jpg');
+
+    $user = User::factory()->create();
+
+    $respose = actingAs($user)->post('/admin/clients', [
+        'name' => 'Manuel Fernandes',
+        'email' => 'manuel@manuelfernandes.com',
+        'photo' => $file
+    ]);
+
+    $this->assertDatabaseHas('clients',[
+        'name' => 'Manuel Fernandes',
+        'email' => 'manuel@manuelfernandes.com'
+    ]);
+
+    $client = Client::where('email', 'manuel@manuelfernandes.com')->first();
+
+    $this->assertNotNull($client->photo);
+
+    actingAs($user)->get(url('client/' . $client->photo))->assertStatus(200);
+
+    Storage::fake();
+});
